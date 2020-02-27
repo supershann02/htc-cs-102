7905505967b4d3f5c9c0dafbc7fecea1f8bd7b84
Shannon <supershann02@gmail.com>
2020-02-26 19:08:48 -0500

    Week 3 Code Along

---
+++ Week 3 Code Along/Week 3 Code Along/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week_3_Code_Along
+{
+    class Dog
+    {
+        public string Breed;
+        public int Height;
+        public string Name;
+
+        public void Bark()
+        {
+            MessageBox.Show("Bark");
+        }
+
+        public void SayName()
+        {
+            MessageBox.Show("My name is " + Name);
+        }
+    }
+}
